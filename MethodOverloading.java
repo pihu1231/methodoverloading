63c209c829f35d86e9eaa07d472b6aa6f8578347
pihu1231 <93186329+pihu1231@users.noreply.github.com>
2022-09-26 12:17:31 +0530

    Add files via upload

---
+++ MethodOverloading.java
@@ -0,0 +1,11 @@
+package CoreJava;
+
+class MethodOverloading {
+ int  over(int a,int b) {
+	return a+b;
+	
+}
+ double over(double a, double b) {
+	return a+b;
+}
+}
---
+++ Overloading.java
@@ -0,0 +1,13 @@
+package CoreJava;
+
+
+
+class Overloading extends MethodOverloading{
+		public static void main(String [] args) {
+			Overloading obj = new Overloading ();
+			System.out.println(obj.over(1,2));
+			System.out.println(obj.over(1,1.2));
+		}
+
+	}
+
